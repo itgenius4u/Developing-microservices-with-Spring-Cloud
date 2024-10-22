ea7eef988c2392553d6d2c333263993eefdb8e14
dennis <itgenius1004@gmail.com>
2024-10-22 09:43:31 +0900

    ok

--- myapp2/src/main/java/com/example/myapp2/repository/UserRepository.java
+++ myapp2/src/main/java/com/example/myapp2/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.example.myapp2.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.myapp2.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 }
